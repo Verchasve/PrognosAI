fd5ab14db3623a3375c0b5a32674d5019f115244
ABHISHEK SINGH <abhiswee@gmail.com>
2025-01-09 16:14:23 -0500

    Adding some mockup file

--- Services/ml_model/predict_model.py
+++ Services/ml_model/predict_model.py
@@ -7,6 +7,14 @@ with open("resolution_model.pkl", "rb") as model_file:
 with open("vectorizer.pkl", "rb") as vectorizer_file:
     vectorizer = pickle.load(vectorizer_file)
 
+print("Model and vectorizer loaded successfully.")
+
+
+# Example new data
+new_data = [
+    "no description incident",
+]
+
 # print("Vectorizer vocabulary:", vectorizer.get_feature_names_out())
 
 # Load the pipeline
@@ -15,7 +23,7 @@ with open("vectorizer.pkl", "rb") as vectorizer_file:
  
 
 #vectorizer = pipeline.named_steps['vectorizer'] 
-print("Model and vectorizer loaded successfully.")
+
 
 # Print the vectorizer vocabulary
 print("Vectorizer vocabulary:")
@@ -24,45 +32,31 @@ print(vectorizer.get_feature_names_out())
 new_terms = ["bug", "feature", "crypto", "user"]
 for term in new_terms:
     print(f"'{term}' in vocabulary: {term in vectorizer.get_feature_names_out()}")
-
-
-# Example new data
-new_data = [
-    "This is a bug report",
-    "Feature request for the crypto module",
-    "User feedback on the latest update"
-]
-
+ 
 # Transform new data using the loaded vectorizer
-new_features = vectorizer.transform(new_data)
-
-# # Print the transformed features array
-print("Transformed features array:")
-print(new_features.toarray())
-
-# print("New data transformed successfully.")
-
-# # Make predictions on the new data
-predictions = model.predict(new_features)
-
-# Predict directly
-#predictions = pipeline.predict(new_data)
-
 new_features = vectorizer.transform(new_data)
 print("Transformed feature vectors (sparse matrix):")
 print(new_features)
+
 print("Transformed feature vectors (dense array):")
 print(new_features.toarray())
+  
 
-
+# Make predictions on the new data
+predictions = model.predict(new_features)
+ 
 
 # Print the numerical predictions
 print("Predictions:", predictions)
 
+# Predict directly
+#predictions = pipeline.predict(new_data) 
+ 
+
 # Define the mapping from numerical labels to text labels
 label_mapping = {
     0: "No match",
-    1: "bug",
+    1: "Bug/Defect",
     2: "Feature",
     3: "User",
     4: "Incident",
